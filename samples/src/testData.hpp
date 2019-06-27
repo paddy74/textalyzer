790e94cf524e2a86403a0f3501a36ab0c8c2adf7
Patrick Cox <coxpatrick74@gmail.com>
2019-06-27 16:20:52 -0400

    Create samples/

---
+++ samples/src/testData.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <string>
+
+std::string getTestData()
+{
+    return "The quick brown fox jumps over the lazy dog. Amazingly few "
+           "discotheques provide jukeboxes.";
+}
